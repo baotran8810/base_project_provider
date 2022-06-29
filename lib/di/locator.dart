cc8ef3687af448583dc626c948e51145d4080fca
baotran8810 <baotran8810@gmail.com>
2022-06-29 14:21:09 +0700

    init base model

---
+++ lib/di/locator.dart
@@ -0,0 +1,11 @@
+import 'package:get_it/get_it.dart';
+
+final locator = GetIt.instance;
+
+void setupLocator() {
+  // Services
+
+  // Repository
+
+  // Models
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:base_project_provider/data/enum.dart';
+import 'package:base_project_provider/di/locator.dart';
+import 'package:flutter/material.dart';
+
+class AppConfig {
+  String? appName;
+  Environment? env;
+  String? baseUrl;
+
+  AppConfig({
+    this.appName,
+    this.env,
+    this.baseUrl,
+  });
+}
+
+void main() {
+  final config =
+      AppConfig(appName: "App Name", env: Environment.dev, baseUrl: "");
+  executeMain(config);
+}
+
+void executeMain(AppConfig config) async {
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    setupLocator();
+    runApp(PokerageApp(config));
+  }, (error, stack) {});
+}
