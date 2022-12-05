3478b2c002bd97194135bc972a2953c9387f1694
baotran8810 <baotran8810@gmail.com>
2022-12-05 16:17:52 +0700

    Update locator

--- lib/di/locator.dart
+++ lib/di/locator.dart
@@ -1,5 +1,11 @@
 import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+import 'locator.config.dart';
 
 final locator = GetIt.instance;
 
-void setupLocator() {}
+@injectableInit
+Future<void> configureDependencies() async {
+  locator.init();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
 }
 
 void executeMain(AppConfig appConfig) async {
-  setupLocator();
+  configureDependencies();
   runApp(
     MyApp(
       appConfig: appConfig,
